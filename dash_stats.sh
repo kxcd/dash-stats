f6b7b546a8e844e0871b852a9d966ff110d4856f
Michael <xkcd@optusnet.com.au>
2026-02-10 18:42:45 +1100

    Updates
    More features, more better.

--- dash_stats.sh
+++ dash_stats.sh
@@ -19,22 +19,11 @@ light_magenta="\e[95m"
 light_cyan="\e[96m"
 white="\e[97m"
 
-# Checks that the required software is installed on this machine.
-bc -v >/dev/null 2>&1 || progs+=" bc"
-jq -V >/dev/null 2>&1 || progs+=" jq"
-
-if [[ -n $progs ]];then
-	text="Missing applications on your system, please run\n\n"
-	text+="\tsudo apt install $progs\n\nbefore running this program again."
-	echo -e "$text" >&2
-	exit 1
-fi
+tabs 8
 
 # The current block found
 height=$(dash-cli getblockcount)
 if (( $? != 0 ));then echo "Problem running dash-cli exiting...";exit 1;fi
-
-date +"%t%F %X"
 
 # From now on, hard exit on any further errors.
 set -e
@@ -66,8 +55,8 @@ difficulty=$(echo "scale=2; $(dash-cli getblockchaininfo|jq -r '.difficulty')/10
 echo -e "$light_blue$difficulty$none\tDifficulty (M)"
 
 # Outstanding coin supply in millions.  Very expensive query to run!
-COINS=10.22
-COINS_TIME=1626245044
+COINS=12.58
+COINS_TIME=1770705478
 if ((COINS_TIME + 3600 < EPOCHSECONDS));then
 	COINS=$(echo "scale=2; $(dash-cli gettxoutsetinfo|jq -r '.total_amount')/1000000"|bc)
 	sed -i "s/\(^COINS_TIME=\).*/\1$EPOCHSECONDS/" "$0"
@@ -115,19 +104,27 @@ echo -e "$light_blue$avg_tx_sec$none\tAverage number of TXes/second"
 
 # Collaterised Masternode count
 masternode=$(dash-cli masternode count)
-masternode_count=$(jq .total<<<"$masternode")
-echo -e "$light_blue$masternode_count$none\tCollateralised MNs"
+masternode_count=$(jq .detailed.regular.total<<<"$masternode")
+#echo -e "$light_blue$masternode_count$none\tCollateralised 1k MNs"
 
 
 
 # List of valid DIP3 Masternode TX hashes
-enabled=$(jq .enabled<<<"$masternode")
-echo -e "$light_blue$enabled$none\tENABLED MNs"
+enabled=$(jq .detailed.regular.enabled<<<"$masternode")
+#echo -e "$light_blue$enabled$none\tENABLED 1k MNs"
+echo -e "$light_blue$masternode_count/$enabled$none 1k MNs (Total/Enabled)"
+
+masternode_count_4k=$(jq .detailed.evo.total<<<"$masternode")
+#echo -e "$light_blue$masternode_count_4k$none\tCollateralised 4k MNs"
+
+enabled_4k=$(jq .detailed.evo.enabled<<<"$masternode")
+#echo -e "$light_blue$enabled_4k$none\tENABLED 4k MNs"
+echo -e "$light_blue$masternode_count_4k/$enabled_4k$none\t4k MNs (Total/Enabled)"
 
 
 # Average age of a masternode in days, similar to coin days destroyed.
-MN_AGE=561.865
-MN_AGE_TIME=1626247591
+MN_AGE=1212.09
+MN_AGE_TIME=1770708567
 if ((MN_AGE_TIME + 1200 < EPOCHSECONDS));then
 	# This RPC call is expensive, so only call it infrequently.
 	protx_list=$(dash-cli protx list valid 1)
@@ -138,8 +135,32 @@ fi
 echo -e "$light_blue$MN_AGE$none\tAverage age of MN (Days)"
 
 
-#enabled=4440
 
+
+# Next subsidy reduction
+daysToSubsidyReduction(){
+	#local block_time=2.625
+	local halving_block=1261441
+	local halving_interval=210240
+	local blocks_til_reduction
+	local days
+	local len
+	blocks_til_reduction=$((halving_interval-((height-halving_block)%halving_interval)))
+	#days=$(bc <<< "scale=1;$blocks_til_reduction*$block_time/60/24")
+	# Avoid use of bc by changing the calculation.
+	days=$((blocks_til_reduction*1000/54857))
+	# Divide by 10 again here.
+	len=$((${#days}-1))
+	echo "${days:0:$len}.${days:$len}"
+	#echo "$days"
+}
+echo -e "$light_blue$(daysToSubsidyReduction)$none\tDays til subsidy reduction"
+
+
+
+
+#enabled_weight=4170
+enabled_weight=$((enabled+enabled_4k*4))
 
 # Variables for calculating the yearly ROI
 FIRST_REALLOC=1379128
@@ -158,27 +179,59 @@ current_block=$height
 blocks_per_year=$(printf '%.0f' $(bc<<<"scale=4;60/$BLOCK_TIME*24*365.25"))
 last_block_of_the_year=$((current_block+blocks_per_year))
 
+calcRoiPreV20(){
+	# General idea compute the reward for every n blocks and accumulate it.
+	# where n is the number of enabled MNs.
+	reward=0
+	for((block=current_block; block<last_block_of_the_year; block+=enabled_weight));do
 
-# General idea compute the reward for every n blocks and accumulate it.
-# where n is the number of enabled MNs.
-reward=0
-for((block=current_block; block<last_block_of_the_year; block+=enabled));do
+		# Find which halving period we are on
+		blocks_since_halving=$((block-FIRST_REWARD_BLOCK))
+		halving_period=$((blocks_since_halving/HALVING_INTERVAL))
 
-	# Find which halving period we are on
-	blocks_since_halving=$((block-FIRST_REWARD_BLOCK))
-	halving_period=$((blocks_since_halving/HALVING_INTERVAL))
+		# Find which realloc period we are on.
+		blocks_since_realloc=$((block-FIRST_REALLOC))
+		# We start counting our periods from zero, each realloc period lasts for 3 super blocks.
+		period=$((blocks_since_realloc/(SUPER_BLOCK_CYCLE*3)))
+		if ((period>18));then
+			period=18
+		fi
 
-	# Find which realloc period we are on.
-	blocks_since_realloc=$((block-FIRST_REALLOC))
-	# We start counting our periods from zero, each realloc period lasts for 3 super blocks.
-	period=$((blocks_since_realloc/(SUPER_BLOCK_CYCLE*3)))
-	if ((period>18));then
-		period=18
-	fi
+		# Combined bc statement.
+		reward=$(echo "scale=8;base_reward=$STARTING_BLOCK_REWARD * (1 - $HALVING_REDUCTION_AMOUNT)^$halving_period;new_reward=base_reward / 500 * ${REALLOC_AMOUNT[$period]};$reward+new_reward"|bc -l)
+	done
+}
 
-	# Combined bc statement.
-	reward=$(echo "scale=8;base_reward=$STARTING_BLOCK_REWARD * (1 - $HALVING_REDUCTION_AMOUNT)^$halving_period;new_reward=base_reward / 500 * ${REALLOC_AMOUNT[$period]};$reward+new_reward"|bc -l)
-done
+calcRoiPostV20(){
+	reward=0
+	block_reward_v20=1.53978154
+	FIRST_REWARD_BLOCK=1892161
+
+	for((block=current_block; block<last_block_of_the_year; block+=enabled_weight));do
+		# Find which halving period we are on
+		blocks_since_halving=$((block-FIRST_REWARD_BLOCK))
+		halving_period=$((blocks_since_halving/HALVING_INTERVAL))
+
+		reward=$(echo "scale=8;base_reward=$block_reward_v20 * (1 - $HALVING_REDUCTION_AMOUNT)^$halving_period;$reward+base_reward"|bc -l)
+	done
+}
+
+calcRoiPostV21(){
+	# Re-compute this var to the new rules where the eMN gets paid only once per cycle.
+	enabled_weight=$((enabled+enabled_4k))
+	calcRoiPostV20
+	reward=$(echo "scale=8;$reward * 0.625"|bc -l)
+}
+
+# Decide which version to use for the calculation of ROI.
+if ((current_block<1987776));then
+	calcRoiPreV20
+elif ((current_block<2128895));then
+	calcRoiPostV20
+else
+	calcRoiPostV21
+fi
+
 
 roi=$(echo "scale=2;$reward/10"|bc)
 echo -e "$light_blue${roi}%$none\tMasternode annualised gross rate of return %"
@@ -188,123 +241,97 @@ echo -e "$light_blue${roi}%$none\tMasternode annualised gross rate of return %"
 
 
 
-
-
-
-dash_data=$(curl -s https://api.coinpaprika.com/v1/tickers/dash-dash?quotes=USD,BTC)
-# Only attempt to print the price when the call returns data and that data is not an error.
-if [[ -n $dash_data && ! $dash_data =~ "503 Service Unavailable" ]];then
-	price_usd=$(printf '$%.2f' $(jq .quotes.USD.price<<<$dash_data))
-	percent_change=$(printf '%.2f%%' $(jq .quotes.USD.percent_change_24h<<<$dash_data))
-	grep -q \- <<<"$percent_change"&&percent_change=$red$percent_change$none||percent_change=$green$percent_change$none
-	price_btc=$(printf '%.4f'  $(jq .quotes.BTC.price<<<$dash_data))
-	echo -e "$light_blue$price_usd$none ($percent_change 24 hr change)  /  $light_blue$price_btc$none BTC"
-fi
-
-
-blockchaininfo=$(dash-cli getblockchaininfo)
-status=$(jq -r '.bip9_softforks.dip0020.status'<<<"$blockchaininfo")
-case $status in
-	defined)
-		echo -e "dip0020 hardfork has not yet started."
-		;;
-	started)
-		windowBlocks=$(jq -r '.bip9_softforks.dip0020.windowBlocks'<<<"$blockchaininfo")
-		windowStart=$(jq -r '.bip9_softforks.dip0020.windowStart'<<<"$blockchaininfo")
-		count=$(jq -r '.bip9_softforks.dip0020.statistics.count'<<<"$blockchaininfo")
-		elapsed=$(jq -r '.bip9_softforks.dip0020.statistics.elapsed'<<<"$blockchaininfo")
-		forkPercent=$(echo "scale=4;$count/$elapsed*100"|bc)
-		forkPercent=$(printf '%.2f%%' $forkPercent)
-		threshold=$(jq -r '.bip9_softforks.dip0020.statistics.threshold'<<<"$blockchaininfo")
-		period=$(jq -r '.bip9_softforks.dip0020.statistics.period'<<<"$blockchaininfo")
-		requiredPercent=$(echo "scale=4;$threshold/$period*100"|bc)
-		requiredPercent=$(printf '%.2f%%' $requiredPercent)
-		progressPercent=$(echo "scale=4;$elapsed/$period*100"|bc)
-		progressPercent=$(printf '%.2f%%' $progressPercent)
-		echo -e "$light_blue$forkPercent$none\tMiners signalling for dip0020 HF readiness %"
-		echo -e "$light_blue$requiredPercent$none\tTarget required for this window %"
-		echo -e "$light_blue$progressPercent$none\tProgress through this window %"
-		;;
-	locked_in)
-		since=$(jq -r '.bip9_softforks.dip0020.since'<<<"$blockchaininfo")
-		echo "The dip0020 hardfork is locked in since block $since"
-		;;
-	active)
-		echo "Hard fork is active."
-		;;
-	*)
-		echo "Hard fork unhandled status."
-		;;
-esac
-
-
-
-
-# First arg is the time in minutes.
-# Second arg is V for vote time, and S for super block time
-_convert_time_units(){
-	if (( $# != 2 ));then return 1;fi
-	if (( $(echo "$1>2880"|bc -l) ));then
-		_TIME=$(echo "scale=2;$1/60/24"|bc)
-		_UNITS="days"
-	elif (( $(echo "scale=2;$1>300"|bc -l) ));then
-		_TIME=$(echo "$1/60"|bc)
-		_UNITS="hours"
-	else
-		# Nothing to convert, just return
-		return 0
-	fi
-	case $2 in
-		S)
-			S_TIME="$_TIME";S_UNITS="$_UNITS"
+hardfork(){
+	fork="withdrawals"
+	blockchaininfo=$(dash-cli getblockchaininfo)
+	status=$(jq -r ".softforks.$fork.bip9.status"<<<"$blockchaininfo")
+	case $status in
+		defined)
+			echo -e "$fork hardfork has not yet started."
 			;;
-		V)
-			V_TIME="$_TIME";V_UNITS="$_UNITS"
+		started)
+			#windowBlocks=$(jq -r ".softforks.$fork.bip9.tatistics.period"<<<"$blockchaininfo")
+			#windowStart=$(jq -r ".softforks.$fork.bip9.windowStart"<<<"$blockchaininfo")
+			count=$(jq -r ".softforks.$fork.bip9.statistics.count"<<<"$blockchaininfo")
+			elapsed=$(jq -r ".softforks.$fork.bip9.statistics.elapsed"<<<"$blockchaininfo")
+			forkPercent=$(echo "scale=4;$count/$elapsed*100"|bc)
+			forkPercent=$(printf '%.2f%%' $forkPercent)
+			threshold=$(jq -r ".softforks.$fork.bip9.statistics.threshold"<<<"$blockchaininfo")
+			period=$(jq -r ".softforks.$fork.bip9.statistics.period"<<<"$blockchaininfo")
+			requiredPercent=$(echo "scale=4;$threshold/$period*100"|bc)
+			requiredPercent=$(printf '%.2f%%' $requiredPercent)
+			progressPercent=$(echo "scale=4;$elapsed/$period*100"|bc)
+			progressPercent=$(printf '%.2f%%' $progressPercent)
+			echo -e "$light_blue$forkPercent$none\tMiners signalling for $fork HF readiness %"
+			echo -e "$light_blue$requiredPercent$none\tTarget required for this window %"
+			echo -e "$light_blue$progressPercent$none\tProgress through this window %"
+			;;
+		locked_in)
+			since=$(jq -r ".softforks.$fork.bip9.since"<<<"$blockchaininfo")
+			echo "The $fork hardfork is locked in since block $since ($((since+4032-height)))"
+			;;
+		active)
+			echo "Hard fork is active."
 			;;
 		*)
-			return 2
+			echo "Hard fork unhandled status."
 			;;
 	esac
 }
+#hardfork
+
+price(){
+	dash_data=$(curl -s https://api.coinpaprika.com/v1/tickers/dash-dash?quotes=USD,BTC)
+	# Only attempt to print the price when the call returns data and that data is not an error.
+	if [[ -n $dash_data && ! $dash_data =~ "503 Service Unavailable" ]];then
+		price_usd=$(jq .quotes.USD.price<<<$dash_data)
+		# Test for a valid number.
+		regex="^[-+]?[0-9]+\.?[0-9]*$"
+		[[ $price_usd =~ $regex ]] || return
+		price_usd=$(printf '$%.2f' $price_usd)
+		percent_change=$(jq .quotes.USD.percent_change_24h<<<$dash_data)
+		[[ $percent_change =~ $regex ]] || return
+		percent_change=$(printf '%.2f%%' $percent_change)
+		grep -q \- <<<"$percent_change"&&percent_change=$red$percent_change$none||percent_change=$green$percent_change$none
+		price_btc=$(jq .quotes.BTC.price<<<$dash_data)
+		[[ $price_btc =~ $regex ]] || return
+		price_btc=$(printf '%.4f'  $price_btc)
+		echo -e "$light_blue$price_usd$none ($percent_change 24 hr change)  /  $light_blue$price_btc$none BTC"
+	fi
+}
+price
+
+_convert_time_units()(
+	local time_num
+	local time_units
+	if(($1>1200));then
+        time_num=$(bc <<< "scale=3;$1*$BLOCK_TIME/60/24")
+        time_units="days"
+    elif(($1>120));then
+		time_num=$(bc <<< "scale=3;$1*$BLOCK_TIME/60")
+		time_units="hours"
+	else
+		time_num=$(bc <<< "scale=3;$1*$BLOCK_TIME")
+		time_units="minutes"
+	fi
+	time_num=$(printf '%.1f' $time_num)
+	echo "$time_num $time_units"
+)
 
 superblock(){
-	# A super block occurs every 16616 blocks
-	SUPER_BLOCK_INTERVAL=16616
-	# The voting deadline occurs 1662 blocks before the super block.
-	VOTING_DEADLINE=1662
-	# A Super block from the past.
-	SUPER_BLOCK=880648
-	# The time taken to create a new block
-	BLOCK_TIME=2.625
-	S_UNITS="minutes";V_UNITS="minutes"
-
-	CURRENT_BLOCK=$height
-	if [[ -z "$CURRENT_BLOCK" || "$CURRENT_BLOCK" -lt 1 ]];then
-		echo "Cannot determine current block, exiting..."
-		return 1;
+	local govinfo
+	local superblockmaturitywindow
+	local nextsuperblock
+	local gap
+	govinfo=$(dash-cli getgovernanceinfo)
+	superblockmaturitywindow=$(jq '.superblockmaturitywindow' <<< "$govinfo")
+	nextsuperblock=$(jq '.nextsuperblock' <<< "$govinfo")
+	gap=$((nextsuperblock-height-superblockmaturitywindow))
+	if((gap>0));then
+		echo "Voting deadline is in $(_convert_time_units $gap) and the next super block will be in $(_convert_time_units $((nextsuperblock-height)))."
+	else
+		echo "Voting deadline has passed, the next super block will be in $(_convert_time_units $((gap+superblockmaturitywindow)))."
 	fi
-
-	while : ;do
-		if ((SUPER_BLOCK-VOTING_DEADLINE-CURRENT_BLOCK < 0));then
-			if ((SUPER_BLOCK-CURRENT_BLOCK < 0));then
-				((SUPER_BLOCK+=SUPER_BLOCK_INTERVAL))
-			else
-				S_TIME=$(echo "$((SUPER_BLOCK-CURRENT_BLOCK))* $BLOCK_TIME"|bc)
-				_convert_time_units "$S_TIME" S
-				echo "Voting deadline has passed, the next super block will be in $S_TIME $S_UNITS."
-				break
-			fi
-		else
-			S_TIME=$(echo "$((SUPER_BLOCK-CURRENT_BLOCK))* $BLOCK_TIME"|bc)
-			_convert_time_units "$S_TIME" S
-			V_TIME=$(echo "$((SUPER_BLOCK-VOTING_DEADLINE-CURRENT_BLOCK))* $BLOCK_TIME"|bc)
-			_convert_time_units "$V_TIME" V
-			echo "Voting deadline is in $V_TIME $V_UNITS and the next super block will be in $S_TIME $S_UNITS."
-			break
-		fi
-	done
-	unset SUPER_BLOCK SUPER_BLOCK_INTERVAL CURRENT_BLOCK S_TIME S_UNITS BLOCK_TIME VOTING_DEADLINE V_TIME V_UNITS _TIME _UNITS
 }
 superblock
-
 
